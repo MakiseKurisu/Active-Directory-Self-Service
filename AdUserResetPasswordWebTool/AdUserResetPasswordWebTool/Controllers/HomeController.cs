42578d86f89a6a49b1818bbed92b3ae9c2e16965
Ami Adi <amiadi1983@outlook.co.il>
2017-06-13 11:07:43 +0300

    First Stage

---
+++ AdUserResetPasswordWebTool/AdUserResetPasswordWebTool/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdUserResetPasswordWebTool.Models;
+using System.DirectoryServices.AccountManagement;
+
+namespace AdUserResetPasswordWebTool.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var model = new ResetPasswordModel();
+
+            return View(model);
+        }
+
+        // POST: Home: Index
+        [HttpPost]
+        public ActionResult Index(ResetPasswordModel model)
+        {
+            try
+            {
+                var pContext = new PrincipalContext(ContextType.Domain);
+                var usrPrincipal = UserPrincipal.FindByIdentity(pContext, model.UserPrincipalName);
+
+                if (usrPrincipal == null)
+                {
+                    throw new Exception("User principal was not found!");
+                }
+
+                usrPrincipal.ChangePassword(model.CurrentPassword, model.NewPassword);
+                usrPrincipal.Save();
+
+                return RedirectToAction("Index", new { msg = 1 });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("ActiveDirectory", ex.Message);
+                if (model == null)
+                {
+                    model = new ResetPasswordModel();
+                }
+                return View(model);
+            }
+        }
+    }
+}
---
+++ AdUserResetPasswordWebTool/AdUserResetPasswordWebTool/Models/ResetPasswordModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdUserResetPasswordWebTool.Models
+{
+    public class ResetPasswordModel
+    {
+        [Required, Display(Name = "User Principal Name")]
+        public string UserPrincipalName { get; set; }
+
+        [Required, Display(Name = "Current Password")]
+        public string CurrentPassword { get; set; }
+
+        [Required, Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Compare("NewPassword"), Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}

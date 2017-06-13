ef915c28ee427a0156300c4ee772600ff531e533
Ami Adi <amiadi1983@outlook.co.il>
2017-06-13 12:31:01 +0300

    Update Author Before Publishing.

--- AdUserResetPasswordWebTool/AdUserResetPasswordWebTool/Properties/AssemblyInfo.cs
+++ AdUserResetPasswordWebTool/AdUserResetPasswordWebTool/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdUserResetPasswordWebTool")]
+[assembly: AssemblyTitle("Self-Service For Active Directory")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdUserResetPasswordWebTool")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("AI-Software")]
+[assembly: AssemblyProduct("Active Directory Self-Service")]
+[assembly: AssemblyCopyright("Copyright ©  2017, Ami Adi - AI-Software All Rights Reserved.")]
+[assembly: AssemblyTrademark("Ami Adi - AI-Software")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 

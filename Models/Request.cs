ecd2c880969b2d7c549d0c4eaa79266982f9c9cf
Makise Kurisu <saberconer@gmail.com>
2018-06-12 07:41:56 -0500

    Add database structure

---
+++ Models/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NPoco;
+
+namespace AdUserResetPasswordWebTool.Models
+{
+    [TableName("Request"), PrimaryKey("RequestID")]
+    public class Request
+    {
+        public Guid RequestID { set; get; }
+        public string UserID { set; get; }
+        public DateTime Date { set; get; }
+    }
+}

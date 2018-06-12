4a2bab57cc6fc054c5e50cafe0e22d3cc43eb460
Makise Kurisu <saberconer@gmail.com>
2018-06-12 07:43:44 -0500

    Add new model

---
+++ Models/ForgetPasswordModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdUserResetPasswordWebTool.Models
+{
+    public class ForgetPasswordModel
+    {
+        [Required, Display(Name = "User Principal Name")]
+        public string UserPrincipalName { get; set; }
+    }
+}
